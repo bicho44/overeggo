1689002c32801ca756f14f72bee7feaaa156d29a
Federico <admin@imgdigital.com.ar>
2017-04-30 10:49:30 -0300

    bug in class container

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 <?php
+
 /**
- * The template for displaying the footer.
+* The template for displaying the footer.
  *
  * Contains the closing of the #content div and all content after.
  *
@@ -10,52 +11,67 @@
  */
 $class="align-right";
 ?>
-
-	<div class="containe">
-			<div class="contactus">
-				Acá va el contacto
-			</div>
-		</div>
-
+<div class="container">
+	<div class="contactus">
+		Acá va el contacto
+	</div>
+</div>
 	<div class="wrapfooter">
-		
-	
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			<div class="container">
 				<div class="row">
-					<?php if ( is_active_sidebar( 'footer-1-sidebar' ) ) { ?>
-					<?php $class="col-md-3 align-right"; ?>
-    					<?php dynamic_sidebar( 'footer-1-sidebar' ); ?>
-					<?php } ?>
-					
-					<?php if ( is_active_sidebar( 'footer-2-sidebar' ) ) { ?>
-					<?php $class="col-md-3 align-right"; ?>
-							<?php dynamic_sidebar( 'footer-2-sidebar' ); ?>
-					<?php } ?>
-					<?php if ( is_active_sidebar( 'footer-3-sidebar' ) ) { ?>
-					<?php $class="col-md-3 align-right"; ?>
-							<?php dynamic_sidebar( 'footer-3-sidebar' ); ?>
-					<?php } ?>
-
-					<div class=<?php echo $class;?>>
+					<?php if ( is_active_sidebar( 'footer-1-sidebar' ) ) {
+	?>
+					<?php $class="col-md-3 align-right";
+?>
+    					<?php dynamic_sidebar( 'footer-1-sidebar' );
+?>
+					<?php
+}
+?>
+					<?php if ( is_active_sidebar( 'footer-2-sidebar' ) ) {
+	?>
+					<?php $class="col-md-3 align-right";
+?>
+							<?php dynamic_sidebar( 'footer-2-sidebar' );
+?>
+					<?php
+}
+?>
+					<?php if ( is_active_sidebar( 'footer-3-sidebar' ) ) {
+	?>
+					<?php $class="col-md-3 align-right";
+?>
+							<?php dynamic_sidebar( 'footer-3-sidebar' );
+?>
+					<?php
+}
+?>
+					<div class=<?php echo $class;
+?>>
 					<!-- Menu Social -->
-					<?php get_template_part('template-parts/menu', 'social'); ?>
-
-					<?php if ( is_active_sidebar( 'footer-4-sidebar' ) ) { ?>
-							<?php dynamic_sidebar( 'footer-4-sidebar' ); ?>
-					<?php } ?>
+					<?php get_template_part('template-parts/menu', 'social');
+?>
+					<?php if ( is_active_sidebar( 'footer-4-sidebar' ) ) {
+	?>
+							<?php dynamic_sidebar( 'footer-4-sidebar' );
+?>
+					<?php
+}
+?>
 					</div>
 				</div>
 			</div><!-- End Container -->
 		</footer>
 		<div class="container">
 			<div class="credits row">
-				<?php imgd_credits(); ?>
+				<?php imgd_credits();
+?>
 			</div>
 		</div>
 	</div> <!-- #wrapfooter -->
-
-<?php wp_footer(); ?>
+<?php wp_footer();
+?>
 </div><!-- end Page -->
 </body>
 </html>
